37f7ba6987c5b4fd09fede77d61fdd48ef885efd
im-usb <udaysbaghel@gmail.com>
2025-02-18 11:14:10 +0530

    Refactor request-url.py to improve status code handling logic

--- request-url.py
+++ request-url.py
@@ -24,3 +24,22 @@ for website in websites:
         status[website] = response.status_code
 
 print(status)
+
+
+# for website in websites:
+#     if not website.startswith("https://"):
+#         website = f"https://{website}"
+#     response = requests.get(website)
+#     # print(response.status_code)
+#     if response.status_code >= 500:
+#         status[website] = "Server error responses"
+#     elif response.status_code >= 400:
+#         status[website] = "Client error responses"
+#     elif response.status_code >= 300:
+#         status[website] = "Redirection responses"
+#     elif response.status_code >= 200:
+#         status[website] = "Successful responses"
+#     elif response.status_code >= 100:
+#         status[website] = "Informational responses"
+
+# print(status)
